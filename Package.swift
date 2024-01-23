35229e5449383e3d949267b00fb77365acb40132
mcmikius <mcmikius@icloud.com>
2024-01-23 12:34:20 +0200

    refactor: update package

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DependencyInjection",
-            targets: ["DependencyInjection"]),
+            targets: ["DependencyInjection"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -22,6 +22,6 @@ let package = Package(
             name: "DependencyInjection"),
         .testTarget(
             name: "DependencyInjectionTests",
-            dependencies: ["DependencyInjection"]),
+            dependencies: ["DependencyInjection"])
     ]
 )
